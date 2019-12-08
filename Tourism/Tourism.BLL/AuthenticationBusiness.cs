b78599899c69684a1ac160aa5e057230cb2ca857
VikulRathod <vikul.rathod.net@gmail.com>
2019-12-08 21:45:48 +0530

    Corrected folder names

---
+++ Tourism/Tourism.BLL/AuthenticationBusiness.cs
@@ -0,0 +1,31 @@
+﻿using Tourism.DAL;
+using Tourism.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tourism.BLL
+{
+    public class AuthenticationBusiness
+    {
+        public User AuthenticateUser(User user)
+        {
+            AuthenticationData dal = new AuthenticationData();
+            return dal.AuthenticateUser(user);
+        }
+        
+        public bool IsPasswordResetLinkValid(string uid)
+        {
+            AuthenticationData dal = new AuthenticationData();
+            return dal.IsPasswordResetLinkValid(uid);
+        }
+
+        public bool ChangeUserPassword(string uid, string newPwd)
+        {
+            AuthenticationData dal = new AuthenticationData();
+            return dal.ChangeUserPassword(uid, newPwd);
+        }
+    }
+}
